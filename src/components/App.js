1e65fe3b6239d999175672e28a6082bcbd3b977c
Carmen D <carmengdominguez94@gmail.com>
2021-05-18 13:23:44 +0200

    Change to class

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,20 @@ import Header from "./Header";
 import Joke from "./Joke";
 import Footer from "./Footer";
 
-function App() {
-    return (
-        <div className="container">
-            <Header />
-            <Joke question={"What word is spelled incorrectly in every single dictionary?"} punchline={"Incorrectly."} />
-            <Joke punchline={"People who use selfie sticks really need to have a good, long look at themselves."} />
-            <Joke question={"How do you make the number one disappear?"} punchline={"Add the letter G and it’s “gone”!"} />
-            <Joke punchline={"A dyslexic man walks into a bra."} />
-            <Joke question={"Why dont blind people skydive?"} punchline={"Because it scares the crap out of their dogs."} />
-            <Footer />
-        </div>
-    );
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <Header />
+                <Joke question={"What word is spelled incorrectly in every single dictionary?"} punchline={"Incorrectly."} />
+                <Joke punchline={"People who use selfie sticks really need to have a good, long look at themselves."} />
+                <Joke question={"How do you make the number one disappear?"} punchline={"Add the letter G and it’s “gone”!"} />
+                <Joke punchline={"A dyslexic man walks into a bra."} />
+                <Joke question={"Why dont blind people skydive?"} punchline={"Because it scares the crap out of their dogs."} />
+                <Footer />
+            </div>
+        );
+    }
 }
 
 export default App;
