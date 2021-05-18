d37e89daae216686a952053f4c7e1059c7212bfd
Carmen D <carmengdominguez94@gmail.com>
2021-05-18 12:30:36 +0200

    actual jokes

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,11 @@ function App() {
     return (
         <div className="container">
             <Header />
-            <Joke question={"question"} punchline={"Here is the punchline"} />
-            <Joke punchline={"Here is the punchline"} />
-            <Joke question={"question"} punchline={"Here is the punchline"} />
-            <Joke punchline={"Here is the punchline"} />
-            <Joke question={"question"} punchline={"Here is the punchline"} />
+            <Joke question={"What word is spelled incorrectly in every single dictionary?"} punchline={"Incorrectly."} />
+            <Joke punchline={"People who use selfie sticks really need to have a good, long look at themselves."} />
+            <Joke question={"How do you make the number one disappear?"} punchline={"Add the letter G and it’s “gone”!"} />
+            <Joke punchline={"A dyslexic man walks into a bra."} />
+            <Joke question={"Why dont blind people skydive?"} punchline={"Because it scares the crap out of their dogs."} />
             <Footer />
         </div>
     );
